8a2290269f9dc977ff2e54696608b9611261c33e
zanebshook <zanebshook@students.abtech.edu>
2026-01-31 17:27:20 -0500

    Add files via upload

---
+++ names.py
@@ -0,0 +1,4 @@
+winner = "dale earnhardt"
+print(winner.title())
+print(winner.upper())
+print(winner.lower())
---
+++ number_8.py
@@ -0,0 +1,11 @@
+# addition
+print(1+7)
+
+# multiplication
+print(4*2)
+
+# subtraction
+print(100-92)
+
+# division
+print(16/2)
