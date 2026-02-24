72d1fea7ded4ec0816f3ce97dd1cdbe80227a228
zanebshook <zanebshook@students.abtech.edu>
2026-02-23 21:49:09 -0500

    Add files via upload

---
+++ 5_3_aliens_yellow.py
@@ -0,0 +1,7 @@
+alien_color = "yellow"
+if alien_color == "green" :
+    print("The player has just scored 5 points.")
+elif alien_color == "yellow":
+    print("The player has just scored 10 points.")
+elif alien_color == "red":
+    print("The player has just scored 15 points.")
